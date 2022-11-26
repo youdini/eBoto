e0f91462b80eb4e8ce1059e4b8b52520f80fefe4
youdini <johnlee3156@gmail.com>
2022-11-26 22:33:34 +0800

    update

---
+++ src/screens/SiginIn.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { View, Text, TouchableOpacity, StyleSheet } from "react-native";
+
+const Sigin = () => {
+  return (
+    <View>
+      <Text>Sign in with Google</Text>
+    </View>
+  )
+}
