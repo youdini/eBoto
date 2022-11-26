a4fe1c402e5ae2abdf9210e8fd9230bc6fe33264
youdini <johnlee3156@gmail.com>
2022-11-26 23:31:59 +0800

    last-update-tonight

--- src/screens/SiginIn.js
+++ src/screens/SiginIn.js
@@ -1,10 +1,57 @@
 import React from "react";
 import { View, Text, TouchableOpacity, StyleSheet } from "react-native";
+import Googelicon from "../assets/google/google";
+import Logo from "../assets/logo/logo";
+import LogoCCC from "../assets/logo_ccc/logo_ccc";
 
-const Sigin = () => {
+const Signin = () => {
   return (
     <View>
-      <Text>Sign in with Google</Text>
+      <View>
+
+      </View>
+      <View style={
+        {
+          alignContent: 'center',
+          alignItems: 'center',
+          marginBottom: 100
+        }
+      }>
+        <Logo />
+      </View>
+      <View style={styles.container}>
+        <Text style={
+          {
+            marginBottom: 20,
+            padding: 10,
+            backgroundColor: 'whitesmoke',
+            borderRadius: 9,
+          }
+        }>Sign in with Google</Text>
+        <TouchableOpacity style={styles.google}>
+          <Googelicon />
+        </TouchableOpacity>
+      </View>
     </View>
   )
 }
+
+export default Signin
+
+const styles = StyleSheet.create({
+  google: {
+    padding: 6,
+    borderRadius: 100,
+    backgroundColor: 'whitesmoke',
+    alignItems: 'center',
+    alignSelf: 'center',
+  },
+
+  container: {
+    backgroundColor: '#7FB5FF',
+    paddingHorizontal: 70,
+    paddingVertical: 50,
+    borderRadius: 20,
+
+  }
+})
